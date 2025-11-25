740d070ee5e410c50f7d5db570538e2dc2ef7b16
pramit-coding <manojpanchal7@gmail.com>
2025-11-25 15:31:28 +1000

    project

---
+++ project.py
@@ -0,0 +1,16 @@
+from string import capwords
+import tkinter as tk
+
+window = tk.Tk()
+window.title("Project")
+window.geometry("500x500")
+def random():
+    
+
+    passwords = ["apple", "bannana", "kiwi"]
+    random().print(passwords)
+
+
+window.text(capwords)
+
+window.mainloop()
